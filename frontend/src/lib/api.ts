f2b0fc1660d74c921261253b17feaea18bdd3a76
Abdi <abdibekele2021@gmail.com>
2026-05-25 15:20:02 +0300

    second update of public_url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL 
+export const API_BASE_URL = process.env.PUBLIC_API_URL 
   ? `${process.env.NEXT_PUBLIC_API_URL}/api/v1` 
   : 'http://localhost:5000/api/v1';
 
